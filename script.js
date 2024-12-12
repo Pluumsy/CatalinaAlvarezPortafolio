f770a7d726147c7844e537672f108dd8252b1454
Pluumsy <163587003+Pluumsy@users.noreply.github.com>
2024-12-12 20:27:43 -0300

    Update script.js

--- script.js
+++ script.js
@@ -111,16 +111,3 @@ document.addEventListener("DOMContentLoaded", function () {
     // Llamar a la función para mostrar imágenes
     displayImages();
 
-
-    // Selecciona todos los enlaces dentro del nav y agrega un evento de clic a cada uno
-document.querySelectorAll('nav a').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-        // Previene el comportamiento predeterminado del enlace
-        e.preventDefault();
-        // Obtiene el destino del enlace a través del atributo href
-        const target = document.querySelector(this.getAttribute('href'));
-        // Desplaza suavemente hasta el destino
-        target.scrollIntoView({ behavior: 'smooth' });
-    });
-});
-});
